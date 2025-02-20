d3ed607eaebdf480e3e26538ef2ba6d9415f5290
Adam Frawley <adamfroggy@outlook.com>
2025-02-20 13:37:58 -0700

    Updated app.py to render home.html template

--- app.py
+++ app.py
@@ -1,1 +1,11 @@
-"from flask import Flask\napp = Flask(__name__)\n@app.route('/hello')\ndef hello():\n    return 'Hello, World!'" > app.py
+from flask import Flask, render_template
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
