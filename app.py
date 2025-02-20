76d7e909c3816ad0249d97fcc0e767cb5aa09b36
Adam Frawley <adamfroggy@outlook.com>
2025-02-20 13:32:28 -0700

    Updated app.py file

--- app.py
+++ app.py
@@ -1,1 +1,1 @@
-echo "from flask import Flask\napp = Flask(__name__)\n@app.route('/hello')\ndef hello():\n    return 'Hello, World!'" > app.py
+"from flask import Flask\napp = Flask(__name__)\n@app.route('/hello')\ndef hello():\n    return 'Hello, World!'" > app.py
