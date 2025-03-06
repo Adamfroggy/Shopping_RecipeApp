f6e254ef6f534e74abb931163e2a5c63be4255e7
Adam Frawley <adamfroggy@outlook.com>
2025-03-06 08:17:24 -0700

    Added ingredient model and updated Recipe model

--- models.py
+++ models.py
@@ -1,5 +1,13 @@
 class Recipe:
     def __init__(self, name, ingredients, instructions):
         self.name = name
-        self.ingredients = ingredients
+        self.ingredients = [Ingredient(*ingredient)
+                            # List of Ingredient objects
+                            for ingredient in ingredients]
         self.instructions = instructions
+
+
+class Ingredient:
+    def __init__(self, name, quantity):
+        self.name = name
+        self.quantity = quantity
