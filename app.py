825d2599f5b1344b31f182ce3b2b9d1d9dcc2836
Adam Frawley <adamfroggy@outlook.com>
2025-05-03 08:49:16 -0600

    Added custom 404 page

--- app.py
+++ app.py
@@ -208,6 +208,11 @@ def recipes():
                            recipes=paginated_recipes, page=page)
 
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+
 @app.route('/recipe/<string:name>')
 def recipe_detail(name):
     recipe = next((r for r in recipes if r.name == name), None)
